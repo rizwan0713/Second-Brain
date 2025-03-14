077356bc4313b7d988dbffad698e4511debe1f18
rizwan0713 <rizwansalmanirsa@gmail.com>
2025-03-15 00:24:41 +0530

    signup done

---
+++ dist/index.js
@@ -0,0 +1,48 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const db_1 = require("./db");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post("/api/v1/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    //zod validation and hash passwoprd
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        yield db_1.UserModel.create({
+            username: username,
+            password: password,
+        });
+        res.status(200).json({
+            message: "User signed up"
+        });
+    }
+    catch (error) {
+        res.status(401).json({
+            message: "user already exist"
+        });
+    }
+}));
+// app.post("/api/v1/signin",(req,res) => {
+// })
+// app.post("/api/v1/content",(req,res) => {
+// })
+// app.get("/api/v1/content",(req,res) => {
+// })
+// app.delete("api/v1/content",(req,res) => {
+// })
+// app.get("api/v1/brain/:shareLink" ,(req,res) => {
+// })
+app.listen(3000);
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,60 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
+const JWT_PASSWORD = "!123123";
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup",(req,res) => {
+app.post("/api/v1/signup", async (req, res) => {
+  //zod validation and hash passwoprd
+  const username = req.body.username;
+  const password = req.body.password;
 
-})
+  try {
+    await UserModel.create({
+      username: username,
+      password: password,
+    });
+    res.status(200).json({
+      message: "User signed up",
+    });
+  } catch (error) {
+    res.status(401).json({
+      message: "user already exist",
+    });
+  }
+});
 
-app.post("/api/v1/signin",(req,res) => {
-    
-})
-app.post("/api/v1/content",(req,res) => {
-    
-})
-app.get("/api/v1/content",(req,res) => {
-    
-})
-app.delete("api/v1/content",(req,res) => {
+app.post("/api/v1/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-})
-app.get("api/v1/brain/:shareLink" ,(req,res) => {
-    
-})
+  const existingUser = await UserModel.findOne({
+    username,
+    password,
+  });
+  if (existingUser) {
+    const Token = jwt.sign(
+      {
+        id: existingUser._id, // the user id in inlcuded the inside the token
+      },
+      JWT_PASSWORD   //secret key to sign 
+    );
+  }
+});
+// app.post("/api/v1/content",(req,res) => {
+
+// })
+// app.get("/api/v1/content",(req,res) => {
+
+// })
+// app.delete("api/v1/content",(req,res) => {
+
+// })
+// app.get("api/v1/brain/:shareLink" ,(req,res) => {
+
+// })
+app.listen(3000);
